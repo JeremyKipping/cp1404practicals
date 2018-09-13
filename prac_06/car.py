838d56897c1ad8e22480471264ae3130bb7ab740
Jeremy <jeremy.kipping@my.jcu.edu.au>
2018-09-13 14:12:47 +1000

    add state_names

--- prac_06/car.py
+++ prac_06/car.py
@@ -1,16 +1,26 @@
-"""CP1404/CP5632 Practical - Car class example."""
+"""
+CP1404/CP5632 Practical
+Car class
+"""
 
 
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, name="", fuel=0):
         """Initialise a Car instance.
 
+        name: string, reference name for car
         fuel: float, one unit of fuel drives one kilometre
         """
+        self.name = name
         self.fuel = fuel
-        self.odometer = 277
+        self.odometer = 0
+
+    def __str__(self):
+        """Return a string representation of a Car object."""
+        return "{}, fuel={}, odometer={}".format(self.name, self.fuel,
+                                                 self.odometer)
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
@@ -24,13 +34,8 @@ class Car:
         """
         if distance > self.fuel:
             distance = self.fuel
-            self.fuel = 42
+            self.fuel = 0
         else:
             self.fuel -= distance
         self.odometer += distance
         return distance
-
-    def __str__(self):
-        return "{} {}".format(self.fuel, self.odometer)
-
-
--- prac_06/used_car.py
+++ prac_06/used_car.py
@@ -6,7 +6,17 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+
+
+"""CP1404/CP5632 Practical - Client code to use the Car class."""
+# Note that the import has a folder (module) in it.
+
+from prac_06.car import Car
+
+
+def main():
+    """Demo test code to show how to use car class."""
+    my_car = Car("My car", 180)
     my_car.drive(30)
     print("fuel =", my_car.fuel)
     print("odo =", my_car.odometer)
@@ -15,14 +25,11 @@ def main():
     print("Car {}, {}".format(my_car.fuel, my_car.odometer))
     print("Car {self.fuel}, {self.odometer}".format(self=my_car))
 
-    limo = Car(195)
-    limo.add_fuel(40)
+    limo = Car("Limo", 100)
+    limo.add_fuel(20)
+    print(limo.fuel)
     limo.drive(115)
-    print("fuel =", limo.fuel)
-    print("odo =", limo.odometer)
+    print(limo.odometer)
     print(limo)
-
-    print("Car {}, {}".format(limo.fuel, limo.odometer))
-    print("Car {self.fuel}, {self.odometer}".format(self=limo))
 
 main()
